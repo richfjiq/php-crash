856b1535554c3613ff96892914bcc494a9c12ce9
richfjiq <rfjiq1986@gmail.com>
2023-06-05 15:17:07 -0600

    php crash course

---
+++ 03_arrays.php
@@ -0,0 +1,46 @@
+<?php
+  $numbers = [1,44,55,22];
+  $fruits = array('apple', 'orange', 'pear');
+
+  // var_dump($numbers);
+  // echo $fruits[1];
+
+  // Associate Array
+  $colors = [
+    1 => 'red',
+    4 => 'blue',
+    6 => 'green'
+  ];
+
+  // echo $colors[4];
+
+  $hex = [
+    'red' => '#f00',
+    'blue' => '#0f0',
+    'green' => '#00f',
+  ];
+
+  // echo $hex['blue'];
+
+  $people = [
+    [
+    'first_name' => 'Brad',
+    'last_name' => 'Stark',
+    'email' => 'test@test.com'
+    ],
+    [
+      'first_name' => 'Ana',
+      'last_name' => 'Zapata',
+      'email' => 'ana@test.com'
+    ],
+    [
+      'first_name' => 'Emy',
+      'last_name' => 'Mora',
+      'email' => 'emy@test.com'
+    ]
+  ];
+
+  // echo $people[1]['first_name'];
+
+  
+?>
